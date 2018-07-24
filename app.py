ac6414b3a5a834e9f751cc849405f81771693bc6
HatemSadallah <hatem200011@gmail.com>
2018-07-24 23:01:44 +0900

    most of the pages

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template("home.html")
+
+@app.route('/yearOne')
+def year_one():
+	return render_template("y1_page.html")
+
+@app.route('/yearTwo')
+def year_two():
+	return render_template("y2_page.html")
+
+@app.route('/yearThree')
+def year_three():
+	return render_template("y3_page.html")
+
+if __name__ == '__main__':
+	app.run()
